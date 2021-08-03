9cc34bd3c58c3a3ac8a88a17a1833f10428414e0
German Diaz <germanrodrigo.2003@hotmail.com>
2021-08-02 23:46:08 -0500

    quito console.logs

--- src/components/ResidentContainer.js
+++ src/components/ResidentContainer.js
@@ -12,7 +12,6 @@ const ResidentContainer = ({residentUrl}) => {
             const url = residentUrl
             const response  = await fetch(url)
             const result = await response.json()
-            console.log(result)
             setData(result)
           }
           locations()
